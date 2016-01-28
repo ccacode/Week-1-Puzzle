c1c61c958d509fd59d86cc04699485c5930f2021
ccacode <tbarry@cca.edu>
2016-01-28 14:12:32 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,31 @@
+var blue=getColor();
+right();right();
+if (getColor() == "blue") {
+ down();
+  right();
+  right();
+} else {
+  up();
+  right();
+  right();
+}
+if (getColor() == "blue") {
+ down();
+  right();
+  right();
+} else {
+  up();
+  right();
+  right();
+}
+if (getColor() == "blue") {
+ down();
+  right();
+  right();
+} else {
+  up();
+  right();
+  right();
+}
+right();
+
